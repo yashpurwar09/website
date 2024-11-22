89f7c5c8bf716915ff8737df87ccf259be9d613c
Pheonix <yashpurwar09@gmail.com>
2024-11-22 22:24:52 +0530

    Added toggle functionality for images.

--- site.js
+++ site.js
@@ -19,3 +19,33 @@ window.addEventListener("resize",()=>{
         nav.style.setProperty("display","none");
     }
 });
+const icon1 = document.querySelector(".icon1");
+const icon2 = document.querySelector(".icon2");
+const icon3 = document.querySelector(".icon3");
+const image1 = document.querySelector(".image1");
+const image2 = document.querySelector(".image2");
+const image3 = document.querySelector(".image3");
+icon1.addEventListener('click',()=>{
+    image1.style.setProperty("display","grid");
+    image2.style.setProperty("display","none");
+    image3.style.setProperty("display","none");
+    icon1.classList.replace("fa-regular","fa-solid");
+    icon2.classList.replace("fa-solid","fa-regular");
+    icon3.classList.replace("fa-solid","fa-regular");
+});
+icon2.addEventListener('click',()=>{
+    image1.style.setProperty("display","none");
+    image2.style.setProperty("display","grid");
+    image3.style.setProperty("display","none");
+    icon1.classList.replace("fa-solid","fa-regular");
+    icon2.classList.replace("fa-regular","fa-solid");
+    icon3.classList.replace("fa-solid","fa-regular");
+});
+icon3.addEventListener('click',()=>{
+    image1.style.setProperty("display","none");
+    image2.style.setProperty("display","none");
+    image3.style.setProperty("display","grid");
+    icon1.classList.replace("fa-solid","fa-regular");
+    icon2.classList.replace("fa-solid","fa-regular");
+    icon3.classList.replace("fa-regular","fa-solid");
+});
